a24292320a2d5ebe5a86f5ff0e07f8ad4fb5ad8c
Benjamin Brinkman <ben@benjaminbrinkman.com>
2013-07-29 00:05:05 -0500

    Added validateEntityType for increased error checking on a parsed entity type
    If a value is passed as an entity type for a Entity and that value
    would normally be out of bounds (e.g. representing neither a PC or an
    NPC) the Entity is reinitialized defaulting to be an NPC.

--- entity.py
+++ entity.py
@@ -8,7 +8,12 @@ class Entity:
     player characters, non-player characters, monsters, creatures, etc."""
     
     def __init__(self, entity_type):
-        self.entity_type = self.parseEntityType(entity_type)
+        temp_entity_type = self.parseEntityType(entity_type)
+        validation_result = self.validateEntityType(temp_entity_type)
+        if validation_result[0] == 'FAIL':
+            self = self.__init__('NPC')
+        else:
+            self.entity_type = temp_entity_type
 
     def parseEntityType(self, entity_type):
         if type(entity_type) is not types.IntType:
@@ -24,3 +29,13 @@ class Entity:
                 return temp_entity_type
         else:
             return entity_type
+    
+    def validateEntityType(self, parsed_entity_type):
+        status = 'OK'
+        i = parsed_entity_type
+        try:
+            et = entity_types[i]
+        except IndexError:
+            status = 'FAIL'
+            et = 'nonexistent'
+        return [status, i, et]
--- tests.py
+++ tests.py
@@ -8,18 +8,32 @@ class EntityTest(unittest.TestCase):
         self.robot = entity.Entity('NPC')
         self.anotherplayer = entity.Entity(0)
         self.anotherrobot = entity.Entity(1)
+        self.invalid = entity.Entity(2)
+        self.test_instance = entity.Entity(1)
     
     def test_init(self):
         self.assertEqual(self.player.entity_type, 0)
         self.assertEqual(self.robot.entity_type, 1)
         self.assertEqual(self.anotherplayer.entity_type, 0)
         self.assertEqual(self.anotherrobot.entity_type, 1)
+        self.assertEqual(self.invalid.entity_type, 1) 
     
     def test_parseEntityType(self):
-        self.assertEqual(self.player.parseEntityType('PC'), 0)
-        self.assertEqual(self.robot.parseEntityType('NPC'), 1)
-        self.assertEqual(self.anotherplayer.parseEntityType(0), 0)
-        self.assertEqual(self.anotherrobot.parseEntityType(1), 1)
+        self.assertEqual(self.test_instance.parseEntityType('PC'), 0)
+        self.assertEqual(self.test_instance.parseEntityType('NPC'), 1)
+        self.assertEqual(self.test_instance.parseEntityType(0), 0)
+        self.assertEqual(self.test_instance.parseEntityType(1), 1)
+
+    def test_validateEntityType(self):
+        i = 0
+        for et in entity.entity_types:
+            self.assertEqual(self.test_instance.validateEntityType(self.player.parseEntityType(et)), ['OK', i, et])
+            self.assertEqual(self.test_instance.validateEntityType(self.player.parseEntityType(i)), ['OK', i, et])
+            i = i + 1
+        
+        for j in range(i, i + 20):
+            self.assertEqual(self.test_instance.validateEntityType(self.player.parseEntityType(i)), ['FAIL', i, 'nonexistent'])
+            i = i + 1
 
 if __name__ == '__main__':
     unittest.main()
